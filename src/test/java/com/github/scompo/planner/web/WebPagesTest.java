48faf9199793a23eb980e642eb7edd123dff99b6
Mauro Scomparin <scompo@gmail.com>
2015-11-21 00:11:43 +0100

    fixed setUp name

--- src/test/java/com/github/scompo/planner/web/WebPagesTest.java
+++ src/test/java/com/github/scompo/planner/web/WebPagesTest.java
@@ -24,7 +24,7 @@ public class WebPagesTest {
 	private MockMvc mockMvc;
 
 	@Before
-	public void setup() {
+	public void setUp() {
 
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
