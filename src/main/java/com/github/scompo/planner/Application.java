a572a01bb2e138301eab00c093646034a94d8a08
Mauro Scomparin <scompo@gmail.com>
2015-11-20 20:35:48 +0100

    Implemented first real test

--- src/main/java/com/github/scompo/planner/Application.java
+++ src/main/java/com/github/scompo/planner/Application.java
@@ -1,13 +1,13 @@
 package com.github.scompo.planner;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
-	/**
-	 * Main entry point.
-	 * 
-	 * @param args command line arguments
-	 */
 	public static void main(String[] args) {
 		
+		SpringApplication.run(Application.class, args);
 	}
 }
--- src/test/java/com/github/scompo/planner/ApplicationTest.java
+++ src/test/java/com/github/scompo/planner/ApplicationTest.java
@@ -1,25 +1,27 @@
 package com.github.scompo.planner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.context.WebApplicationContext;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebIntegrationTest
 public class ApplicationTest {
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	
+	@Autowired
+	private WebApplicationContext wac;
+	
 	@Test
 	public void test() {
 		
-		assertTrue("application never started", true);
+		assertNotNull(wac);
 	}
 
 }
