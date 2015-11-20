ccf2007de2a4587d3c784d824e9d8f333e69c353
Mauro Scomparin <scompo@gmail.com>
2015-11-20 22:00:10 +0100

    added WebPagesTest

---
+++ src/test/java/com/github/scompo/planner/web/WebPagesTest.java
@@ -0,0 +1,14 @@
+package com.github.scompo.planner.web;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WebPagesTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
