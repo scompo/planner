8397177db0e47c253a9671fbbc85dd74d609691b
Mauro Scomparin <scompo@gmail.com>
2015-11-20 20:01:49 +0100

    added comments

--- src/main/java/com/github/scompo/planner/Application.java
+++ src/main/java/com/github/scompo/planner/Application.java
@@ -2,6 +2,11 @@ package com.github.scompo.planner;
 
 public class Application {
 
+	/**
+	 * Main entry point.
+	 * 
+	 * @param args command line arguments
+	 */
 	public static void main(String[] args) {
 		
 	}
