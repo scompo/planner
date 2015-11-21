21389f542e480c07d80277c3307081341abe375c
Mauro Scomparin <scompo@gmail.com>
2015-11-21 01:49:46 +0100

    removed hashcode and equals

--- src/main/java/com/github/scompo/planner/domain/User.java
+++ src/main/java/com/github/scompo/planner/domain/User.java
@@ -7,15 +7,15 @@ import javax.persistence.Id;
  * User implementation.
  */
 @Entity
-public class User{
+public class User {
 
 	@Id
 	private String username;
-	
+
 	private String name;
-	
+
 	private String surname;
-	
+
 	private String email;
 
 	public String getUsername() {
@@ -50,53 +50,6 @@ public class User{
 		this.email = email;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
-		result = prime * result + ((username == null) ? 0 : username.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		}
-		else if (!email.equals(other.email))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		}
-		else if (!name.equals(other.name))
-			return false;
-		if (surname == null) {
-			if (other.surname != null)
-				return false;
-		}
-		else if (!surname.equals(other.surname))
-			return false;
-		if (username == null) {
-			if (other.username != null)
-				return false;
-		}
-		else if (!username.equals(other.username))
-			return false;
-		return true;
-	}
-	
 	@Override
 	public String toString() {
 		return "User [username=" + username + ", name=" + name + ", surname=" + surname + ", email=" + email + "]";
